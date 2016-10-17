afdb593b7645e4e2d145c557b60711ef7ac2a69a
kevin.liu <791845283@qq.com>
2016-10-17 19:34:32 +0800

    新文件

---
+++ App/Common/Conf/relation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/8 0008
+ * Time: 00:08
+ */
+return array(
+  'userRole' => array(
+      '1' => '董事',
+      '2' => '总代理',
+      '3' => '代理商',
+      '4' => '准代理',
+      '5' => '批发商',
+      '6' => '零售商'
+  ),
+
+  'uploadQiniu' => array(
+      /***七牛配置***/
+      'ACCESS_KEY'           => '31BMePSLtnrquPr5uXgOiCii-_S8vVY0pIobQU9V',
+      'SECRET_KEY'           => '9j4-N40OrKM7UelTtijcr9P5Ka_LgTcQfucHQjYG',
+      'BUCKET'               => 'haoli',
+      'QINIUDOMAIN'          => 'http://7xo1wr.com1.z0.glb.clouddn.com',
+
+  )
+);
