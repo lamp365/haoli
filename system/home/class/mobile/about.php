6026ccb18837fb009f4bc04c010bdf3a56e2cd2c
kevin.liu <791845283@qq.com>
2017-07-25 22:01:43 +0800

    about

--- system/home/class/mobile/about.php
+++ system/home/class/mobile/about.php
@@ -13,7 +13,16 @@ class about extends \home\controller\base
 
 	public function cc()
 	{
-		$a = new \twinh\tenpay\Tenpay();
-		$a->index();
+		$tenpay_config = array(
+			'partner' => '1486474181',
+			'key' => '7df7a938fc9449b17f7b9b4f5127c59a',
+			'return_url' => 'http://baidu.com',
+			'notify_url' => 'http://baidu.com',
+		);
+		$a = new \kevin365\tenpay\Tenpay($tenpay_config);
+		$data['ordersn'] = date('YmdHis').uniqid();
+		$data['title']   = '测试商品';
+		$data['price']   = '1';
+		$a->pay($data);
 	}
 }
